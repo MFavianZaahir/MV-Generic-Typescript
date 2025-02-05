22dc283a19d43192381f95657d66940cc2758bf5
MFavianZaahir <favianzaahir@gmail.com>
2025-02-05 19:47:39 +0700

    applied ts generics

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,29 +1,34 @@
 import React from "react";
 
-export type ColumnDefs = {
+export type ColumnDefs<T> = {
     title: string;
 } & (
-        | {
-            field: string;
-        }
-        | {
-            render: (rowData: Record<string, any>) => React.ReactNode;
-        }
-    );
-
-const Table = ({
+    | {
+          field: keyof T;
+          render?: undefined;
+      }
+    | {
+          render: (rowData: T) => React.ReactNode;
+          field?: undefined;
+      }
+);
+const Table = <T,>({
     columnDefs,
     data,
 }: {
-    columnDefs: ColumnDefs[];
-    data: Record<string, any>[];
+    columnDefs: ColumnDefs<T>[];
+    data: T[];
 }) => {
     return (
         <table className="min-w-full divide-y overflow-hidden rounded-lg divide-gray-800 shadow-lg">
             <thead className="bg-green-200">
                 <tr>
                     {columnDefs.map((column, index) => (
-                        <th key={index} scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <th
+                            key={index}
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
                             {column.title}
                         </th>
                     ))}
@@ -31,10 +36,20 @@ const Table = ({
             </thead>
             <tbody className="bg-white divide-y divide-gray-200">
                 {data.map((rowData, index) => (
-                    <tr key={index} className="cursor-pointer hover:bg-gray-100">
+                    <tr
+                        key={index}
+                        className="cursor-pointer hover:bg-gray-100"
+                    >
                         {columnDefs.map((column, index) => (
-                            <td key={index} className="px-6 py-4 whitespace-nowrap">
-                                {"field" in column ? rowData[column.field!] : column.render ? column.render(rowData) : null}
+                            <td
+                                key={index}
+                                className="px-6 py-4 whitespace-nowrap"
+                            >
+                                {"field" in column
+                                    ? String(rowData[column.field as keyof T])
+                                    : column.render
+                                    ? column.render(rowData)
+                                    : null}
                             </td>
                         ))}
                     </tr>
